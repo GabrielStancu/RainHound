a539b5f565df7345163a5f775578a4c43cdaff37
Stancu Gabriel <gabriel.stancu07@yahoo.com>
2023-11-20 00:02:56 +0200

    Updated build & deploy configs

--- RainHound.Client/src/environments/environment.development.ts
+++ RainHound.Client/src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   weatherUrl: 'http://localhost:5253/api/',
   alertsUrl: 'http://localhost:7174/api/',
-  appInsightsInstrumentationKey: 'a30f6e65-4262-4b68-89d0-32fd6b45a8e1'
+  appInsightsInstrumentationKey: '195521b0-4ed7-43fb-a109-b07a26669228'
 };
--- RainHound.Client/src/environments/environment.integration.ts
+++ RainHound.Client/src/environments/environment.integration.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   weatherUrl: 'inte-not-defined',
   alertsUrl: 'inte-not-defined',
-  appInsightsInstrumentationKey: 'a30f6e65-4262-4b68-89d0-32fd6b45a8e1'
+  appInsightsInstrumentationKey: '195521b0-4ed7-43fb-a109-b07a26669228'
 };
