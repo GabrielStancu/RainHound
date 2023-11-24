b241814608f6ceca76fc3872c95bba47a1400b0b
Stancu Gabriel <gabriel.stancu07@yahoo.com>
2023-11-24 22:03:24 +0200

    Environment route

--- RainHound.Client/src/app/app-routing.module.ts
+++ RainHound.Client/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { WeatherComponent } from './components/weather/weather.component';
 import { ForecastComponent } from './components/forecast/forecast.component';
 import { SettingsComponent } from './components/settings/settings.component';
+import { EnvironmentComponent } from './components/environment/environment.component';
 
 const routes: Routes = [
   { path: 'weather', component: WeatherComponent },
   { path: 'forecast', component: ForecastComponent },
   { path: 'settings', component: SettingsComponent },
+  { path: 'environment', component: EnvironmentComponent },
   { path: '', redirectTo: '/weather', pathMatch: 'full' }
 ];
 
