5fe2b378d6e38e5ff14ab826b9499cf74f0c0c93
Stancu Gabriel <gabriel.stancu07@yahoo.com>
2023-12-01 22:38:28 +0200

    Updated web api URL

--- RainHound.Client/src/environments/environment.integration.ts
+++ RainHound.Client/src/environments/environment.integration.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  weatherUrl: 'https://rainhound-weatherapi.cyhpb5c7c8gwckde.switzerlandnorth.azurecontainer.io/api/',
+  weatherUrl: 'https://rainhound-gs-api-inte.azurewebsites.net/api/',
   alertsUrl: 'https://rainhound-gs-alerts-inte.azurewebsites.net/api/',
   setAlertEndpoint: 'SetAlert?code=Kkrea6DzeK_tmWdkyqdY8Hlm_fi-5ygDd2ZLs0J6rATyAzFuTj2JSg==',
   appInsightsInstrumentationKey: '195521b0-4ed7-43fb-a109-b07a26669228',
