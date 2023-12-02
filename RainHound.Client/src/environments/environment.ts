03991e81c68d28b2ca0ec6c5863f4e356bc3e6d6
Stancu Gabriel <gabriel.stancu07@yahoo.com>
2023-12-02 19:00:10 +0200

    Pipelines and credentials

--- RainHound.Client/src/environments/environment.ts
+++ RainHound.Client/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  weatherUrl: 'prod-not-defined',
+  weatherUrl: 'https://rainhound-gs-api.azurewebsites.net/api/',
   alertsUrl: 'prod-not-defined',
   setAlertEndpoint: 'prod-not-defined',
-  appInsightsInstrumentationKey: 'prod-not-defined',
+  appInsightsInstrumentationKey: '95b7ce85-051b-47f3-b7cf-c866ffcacedc',
   environment: 'Production'
 };
