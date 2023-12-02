ddb942e752e1b5bb2defdf5dd238533a58570d06
Stancu Gabriel <gabriel.stancu07@yahoo.com>
2023-12-02 19:27:12 +0200

    Updated client configs

--- RainHound.Client/src/environments/environment.ts
+++ RainHound.Client/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   weatherUrl: 'https://rainhound-gs-api.azurewebsites.net/api/',
-  alertsUrl: 'prod-not-defined',
-  setAlertEndpoint: 'prod-not-defined',
+  alertsUrl: 'https://rainhound-gs-alerts.azurewebsites.net/api/',
+  setAlertEndpoint: 'SetAlert?code=dKNzwcqwN-8quoMpayl_xYMxPFBFWZe_kATT73-AqMSgAzFuooDXZA==',
   appInsightsInstrumentationKey: '95b7ce85-051b-47f3-b7cf-c866ffcacedc',
   environment: 'Production'
 };
